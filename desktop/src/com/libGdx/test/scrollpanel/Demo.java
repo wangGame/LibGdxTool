668ebea7dd14f11f76adc99057ae2da0f987e22e
kangwang <3267846129@qq.com>
2023-09-05 18:45:26 +0800

    滚动大小

---
+++ desktop/src/com/libGdx/test/scrollpanel/Demo.java
@@ -0,0 +1,94 @@
+package com.libGdx.test.scrollpanel;
+
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.scenes.scene2d.Action;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.kw.gdx.constant.Constant;
+import com.libGdx.test.base.LibGdxTestMain;
+
+import kw.artpuzzle.scroll.ScrollPane;
+
+/**
+ * @Auther jian xian si qi
+ * @Date 2023/9/5 18:20
+ */
+public class Demo extends LibGdxTestMain {
+    public static void main(String[] args) {
+        Demo demo = new Demo();
+        demo.start(demo);
+    }
+
+    private float baseX;
+    @Override
+    public void useShow(Stage stage) {
+        super.useShow(stage);
+        Table table = new Table(){
+            {
+                for (int i = 0; i < 100; i++) {
+                    add(new DemoGroup(){
+                        @Override
+                        public void draw(Batch batch, float parentAlpha) {
+                            super.draw(batch, parentAlpha);
+                            float v = getX() - baseX;
+                            setOffSetX(v);
+                        }
+                    });
+                }
+                pack();
+            }
+        };
+        ScrollPane pane = new ScrollPane(table){
+            @Override
+            public void act(float delta) {
+
+                baseX = getScrollX()+Constant.GAMEWIDTH/2;
+                super.act(delta);
+            }
+        };
+        pane.setSize(Constant.GAMEWIDTH,Constant.GAMEHIGHT);
+        addActor(pane);
+
+
+
+        pane.addAction(Actions.sequence(Actions.run(new Runnable() {
+            @Override
+            public void run() {
+                pane.setScrollX(100);
+                pane.updateVisualScroll();
+            }
+        }),Actions.delay(4),
+                Actions.run(new Runnable() {
+                    @Override
+                    public void run() {
+                        pane.setScrollX(0);
+                        pane.updateVisualScroll();
+                    }
+                }),Actions.delay(4),
+                Actions.run(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        pane.setScrollX(29);
+
+                    }
+                }),Actions.delay(4),
+                Actions.run(new Runnable() {
+                    @Override
+                    public void run() {
+                        pane.setScrollX(500);
+
+                    }
+                }),Actions.delay(4),
+                Actions.run(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        pane.setScrollX(400);
+
+                    }
+                })
+                ));
+    }
+}
