26abe922c0495034b49586f3b8ab0bdc1ca57576
kangwang <2818815189@qq.com>
2024-06-23 20:54:34 +0800

    test

--- libGdxLib/src/com/kw/gdx/abtest/ABTest.java
+++ libGdxLib/src/com/kw/gdx/abtest/ABTest.java
@@ -2,12 +2,8 @@ package com.kw.gdx.abtest;
 
 public class ABTest {
     public static String currentV = "B";
-
     public static boolean isVersion(String name){
         if (name==null)return false;
-        if (name.equals("A") && currentV.equals("")){
-            return true;
-        }
         if (currentV.equalsIgnoreCase(name)) {
             return true;
         }else {
