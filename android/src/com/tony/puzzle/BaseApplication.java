cc840d912a29845939b5db79454b25fa0a58ed57
kangwang <2818815189@qq.com>
2025-03-28 23:48:58 +0800

    没啥可写了，只读了

--- android/src/com/tony/puzzle/BaseApplication.java
+++ android/src/com/tony/puzzle/BaseApplication.java
@@ -2,7 +2,15 @@ package com.tony.puzzle;
 
 import android.app.Application;
 
+import androidx.core.view.ViewCompat;
+
 import com.badlogic.gdx.backends.android.AndroidApplication;
 
 public class BaseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+//        ViewCompat.MEASURED_SIZE_MASK;
+        
+    }
 }
