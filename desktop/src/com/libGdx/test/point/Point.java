fbce8f1acc58c2d9069557a69bda213de3b7b0ff
kangwang <2818815189@qq.com>
2025-06-15 20:01:59 +0800

    日志

---
+++ desktop/src/com/libGdx/test/point/Point.java
@@ -0,0 +1,62 @@
+package com.libGdx.test.point;
+
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.utils.Align;
+import com.kw.gdx.asset.Asset;
+import com.libGdx.test.base.LibGdxTestMain;
+
+public class Point extends LibGdxTestMain {
+    public static void main(String[] args) {
+        Point p = new Point();
+        p.start();
+    }
+
+    @Override
+    public void useShow(Stage stage) {
+        super.useShow(stage);
+        init_sine_table();
+
+        int i = 0;
+        for (float v : sine_table) {
+            Image image = new Image(Asset.getAsset().getTexture("assets/next.png"));
+            image.setSize(10,10);
+            image.setPosition(i,v*100+100, Align.center);
+            addActor(image);
+            i+=30;
+        }
+
+        generate_wave();
+        i=0;
+        for (float v : output_buffer) {
+            Image image = new Image(Asset.getAsset().getTexture("assets/next.png"));
+            image.setSize(10,10);
+            image.setPosition(i,v*100+500, Align.center);
+            addActor(image);
+            i+=30;
+        }
+    }
+    int TABLE_SIZE =  20;                     // 正弦波一周期点数 N = 20
+            int  SAMPLE_RATE =  100000;               // Fs = 100kHz
+//            #define SINE_FREQ (SAMPLE_RATE / TABLE_SIZE)  // 输出频率应为 5kHz
+//#define SAMPLE_COUNT 200
+    float sine_table[];
+//    float output_buffer[SAMPLE_COUNT];
+
+    void init_sine_table() {
+        sine_table = new float[TABLE_SIZE];
+        for (int i = 0; i < TABLE_SIZE; i++) {
+            float angle = 2 * 3.1415926f * i / TABLE_SIZE;
+            sine_table[i] = (float) Math.sin(angle);  // 输出 -1~+1
+        }
+    }
+
+    float output_buffer[] =new float[200];
+    void generate_wave() {
+        int index = 0;
+        for (int i = 0; i < 200; i++) {
+            output_buffer[i] = sine_table[index++];
+            if (index >= TABLE_SIZE) index = 0;
+        }
+    }
+}
