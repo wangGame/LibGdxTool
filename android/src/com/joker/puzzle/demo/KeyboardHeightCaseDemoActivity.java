23b76eefaa2a24722903cc24cf1a5d383742eb35
tony <kangwang@doodlemobile.com>
2026-09-03 19:36:09 +0800

    改包名joker  joker头像

--- android/src/com/joker/puzzle/demo/KeyboardHeightCaseDemoActivity.java
+++ android/src/com/joker/puzzle/demo/KeyboardHeightCaseDemoActivity.java
@@ -14,7 +14,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.tony.puzzle.R;
+import com.joker.puzzle.R;
 
 /**
  * Runnable Android demo for keyboard-height behavior.
